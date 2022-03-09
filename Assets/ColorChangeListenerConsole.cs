3b98c8982f4abb0095ca011a2af8f85640f71e56
Lucas Torti <47060346+LucTort@users.noreply.github.com>
2022-03-08 20:10:53 -0800

    Initial commit

---
+++ Assets/ColorChangeListenerConsole.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+public class ColorChangeListenerConsole
+{
+    public ColorChangeListenerConsole()
+    {
+        ColorModel.OnChangeColor += ChangeColorEvent;
+    }
+    ~ColorChangeListenerConsole()
+    {
+        ColorModel.OnChangeColor -= ChangeColorEvent;
+    }
+    void ChangeColorEvent(Color newColor)
+    {
+        Debug.Log("new color = " + newColor);
+    }
+}
